56996a18fa34036f249c6bd6dcbc4863bb4a7dba
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-03-12 16:30:36 -0500

    expanding search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,28 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Header() {
+  const [expand, setExpand] = useState(false);
+  const handleChange = () => {};
   return (
     <header>
       <nav>
-        <img className="search" src="../images/search.png" alt="search" />
+        <div className="search-bar">
+          <img
+            className="search"
+            onClick={() => setExpand(!expand)}
+            src="../images/search.png"
+            alt="search"
+          />
+          {expand && (
+            <form>
+              <input type="search" name="search" onChange={handleChange} />
+            </form>
+          )}
+        </div>
         <div className="nav-bar">
-            <Link to="/bag">Bag</Link>
-            <div className="logo">Logo</div>
+          <Link to="/bag">Bag</Link>
+          <div className="logo">Logo</div>
         </div>
       </nav>
     </header>
