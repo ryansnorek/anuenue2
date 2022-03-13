842a7671e8cb9f81e64cc060378a01f8dbc869af
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-03-13 12:43:42 -0400

    image options / animations

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,8 @@
 import { data } from "../index";
 
 function Item({ contentName }) {
-    const { name, img } = data[contentName];
+  const { name, img } = data[contentName];
+
   return (
     <section className={`item ${contentName}`}>
       <div className="left">
@@ -9,7 +10,12 @@ function Item({ contentName }) {
         <p>item description desceoprpeor jflaksnf aklndan </p>
       </div>
       <div className="right">
-        <img src={img} alt="item"/>
+        <img className="main-image" src={img} alt="item" />
+        <div className="alternate-images">
+          <img src={img} alt="item" />
+          <img src={img} alt="item" />
+          <img src={img} alt="item" />
+        </div>
       </div>
     </section>
   );
