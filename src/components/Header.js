8390b1d40cb117be2c6ca2e973b27dea15a10b56
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-03-16 11:15:15 -0400

    search-bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,8 @@ import useForm from "../hooks/useForm";
 
 function Header() {
   const inputRef = useRef();
-  const { expandSearchBar, setExpandSearchBar, scrollBreakPoint } = useContext(effectsContext);
+  const { expandSearchBar, setExpandSearchBar, scrollBreakPoint } =
+    useContext(effectsContext);
   const [formValues, handleChange, clearForm] = useForm({ search: "" });
   const handleClick = () => setExpandSearchBar(!expandSearchBar);
   const handleSubmit = (e) => clearForm(e);
@@ -17,33 +18,34 @@ function Header() {
     }
   }, [expandSearchBar]);
 
-
-
   return (
     <header className={`${scrollBreakPoint && "scrolled-header"}`}>
-      <img className={`logo ${scrollBreakPoint && "scrolled-logo"}`} src="../images/icons/anuenue_logo.png" alt="logo" />
-
+      <img
+        className={`logo ${scrollBreakPoint && "scrolled-logo"}`}
+        src="../images/icons/anuenue_logo.png"
+        alt="logo"
+      />
       <nav>
-        {/* <div className="search-bar">
-          <img
-            className="search"
-            onClick={handleClick}
-            src="../images/icons/search.png"
-            alt="search"
-          />
-          {expandSearchBar && (
-            <form onSubmit={handleSubmit}>
-              <input
-                ref={inputRef}
-                type="search"
-                name="search"
-                value={formValues.search}
-                onChange={handleChange}
-              />
-            </form>
-          )}
-        </div> */}
         <div className="nav-bar">
+          {/* <div className="search-bar">
+            <img
+              className="search"
+              onClick={handleClick}
+              src="../images/icons/search.png"
+              alt="search"
+            />
+            {expandSearchBar && (
+              <form onSubmit={handleSubmit}>
+                <input
+                  ref={inputRef}
+                  type="search"
+                  name="search"
+                  value={formValues.search}
+                  onChange={handleChange}
+                />
+              </form>
+            )}
+          </div> */}
           <img className="social" src="../images/icons/insta.png" alt="logo" />
           <Link to="/about">About</Link>
           <Link to="/shop">Shop</Link>
