3f2cb2f539f9aa376fba6573306ecd3b87fcfecd
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-03-17 16:40:15 -0400

    use Item components

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,5 @@
 import { data } from "../data";
-import Desserts from "./Desserts";
+import Item from "./Item";
 import Footer from "./Footer";
 
 function Landing() {
@@ -7,36 +7,27 @@ function Landing() {
     <main className="landing">
       <section className="trending">
         <h2>Illuminate your gullet</h2>
-        <div className="items">
-          <div className="item">
-            <img src={data.power_bitch_balls.img} alt="balls" />
-            <h4>Power Bitch Balls</h4>
-          </div>
-          <div className="item">
-            <img src={data.green_burrito.img} alt="burrito" />
-            <h4>Green Brrrrito</h4>
-          </div>
-          <div className="item">
-            <img src={data.all_your_breads.img} alt="bread" />
-            <h4>All Your Bread Are Belong To Us</h4>
+        <div className="item-container">
+          <Item item={data.power_bitch_balls} />
+          <Item item={data.green_burrito} />
+          <Item item={data.all_your_breads} />
+        </div>
+      </section>
+      <section className="intro-card">
+        <div className="card">
+          <img src="../images/intro.png" alt="dessert" />
+          <div className="text-wrapper">
+            <div className="text">
+              <h3>Why hello there</h3>
+              <p>
+                Fancy meeting you here. I'm Erica and this is what I do. I can't
+                wait to show you my delectibles
+              </p>
+            </div>
           </div>
         </div>
       </section>
-      <section className="intro-card">  
-      <div className="card">
-        <img src="../images/intro.png" alt="dessert" />
-        <div className="text-wrapper">
-          <div className="text">
-            <h3>Why hello there</h3>
-            <p>
-              Fancy meeting you here. I'm Erica and this is 
-              what I do. I can't wait to show you my delectibles
-            </p>
-          </div>
-        </div>
-        </div>
-      </section>
-        <Footer />
+      <Footer />
     </main>
   );
 }
