59145b157bda0892e154084f20d2b63a616f3d06
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-03-17 15:54:51 -0400

    shop footer

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,26 +1,30 @@
 import { data } from "../data";
 import Item from "./Item";
+import Footer from "./Footer";
 
 function Shop() {
   return (
-    <div className="shop">
-      <section className="desserts">
-        <h2>Desserts</h2>
-        <div className="item-container">
-            <Item item={data.chonky_chip}/>
-            <Item item={data.based_brownies}/>
-            <Item item={data.they_cookies}/>
-        </div>
-      </section>
-      <section className="desserts">
-        <h2>Pepe Choice</h2>
-        <div className="item-container">
-            <Item item={data.power_bitch_balls}/>
-            <Item item={data.all_your_breads}/>
-            <Item item={data.champs_elycakes}/>
-        </div>
-      </section>
-    </div>
+    <>
+      <div className="shop">
+        <section className="desserts">
+          <h2>Desserts</h2>
+          <div className="item-container">
+            <Item item={data.chonky_chip} />
+            <Item item={data.based_brownies} />
+            <Item item={data.they_cookies} />
+          </div>
+        </section>
+        <section className="desserts">
+          <h2>Pepe Choice</h2>
+          <div className="item-container">
+            <Item item={data.power_bitch_balls} />
+            <Item item={data.all_your_breads} />
+            <Item item={data.champs_elycakes} />
+          </div>
+        </section>
+      </div>
+      <Footer />
+    </>
   );
 }
 export default Shop;
