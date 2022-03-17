67d5c31b8403b707a8bc44d97e494f17add4ac93
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-03-17 16:55:25 -0400

    clean up css, new fonts

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,12 @@
 function Item({ item }) {
   return (
     <div className="item">
-    <img src={item.img} alt="chonky chip" />
-    <div className="text">
-      <h5>{item.name}</h5>
-      <p>{item.price}</p>
+      <img src={item.img} alt="chonky chip" />
+      <div className="text">
+        <h5>{item.name}</h5>
+        <p>${item.price}</p>
+      </div>
     </div>
-  </div>
   );
 }
 export default Item;
