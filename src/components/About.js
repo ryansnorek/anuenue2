d470d7832928a0623c21da7203e0a23487ee5a55
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-03-19 11:59:33 -0400

    about wip

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,16 @@ function About() {
     <div className="about">
       <div className="banner">
         <img src="../images/rainbow.png" alt="banner" />
+        <div className="text">
+            <h2>What is Anuenue?</h2>
+            <p>It's like this feeling in your gut man.
+                I followed this feeling to the depths 
+                of Hana and brought back with me the 
+                eternal wisdom to create gorging material
+                that illuminates the gullet. To bring you
+                the same feeling that inspired my journey.
+            </p>
+        </div>
       </div>
     </div>
   );
