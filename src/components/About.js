aec3b09f3976e487ef500ea4721850e5e1745520
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-03-19 12:07:25 -0400

    about / change class name, add Footer

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,22 @@
+import Footer from "./Footer";
+
 function About() {
   return (
     <div className="about">
-      <div className="banner">
-        <img src="../images/rainbow.png" alt="banner" />
+      <div className="what-is-anuenue">
+        <img className="skeleton" src="../images/rainbow.png" alt="banner" />
         <div className="text">
             <h2>What is Anuenue?</h2>
-            <p>It's like this feeling in your gut man.
+            <p>It's like this feeling in your gut, man.
                 I followed this feeling to the depths 
                 of Hana and brought back with me the 
                 eternal wisdom to create gorging material
-                that illuminates the gullet. To bring you
+                that illuminates the gullet. Now I bring you
                 the same feeling that inspired my journey.
             </p>
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
