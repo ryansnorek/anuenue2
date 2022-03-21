f9eeb3ccbfd6061696dfe585c6fe1f995702dd41
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-03-21 15:19:28 -0400

    bag layout

--- src/components/Bag.js
+++ src/components/Bag.js
@@ -1,20 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { shoppingContext } from "../contexts";
 import BagItem from "./common/BagItem";
 
 function Bag() {
   const { order } = useContext(shoppingContext);
+  const [total, setTotal] = useState(0);
 
-  
+  useEffect(() => {
+    const orderTotal = order.reduce((x, y) => {
+      return x + y.price * y.qty;
+    }, 0);
+    setTotal(orderTotal);
+  }, [order]);
 
   return (
     <div className="bag">
+      <div className="columns">
+        <h3 className="name">Item</h3>
+        <h3 className="quantity">Quantity</h3>
+        <h3 className="total">Total</h3>
+      </div>
       <section className="order">
         {order.map((item) => {
-          return (
-            <BagItem key={item.id} item={item}/>
-          );
+          return <BagItem key={item.id} item={item} />;
         })}
+      </section>
+      <section className="checkout">
+        <div className="total">
+          <h2>Total</h2>
+          <h3>${total}</h3>
+        </div>
       </section>
     </div>
   );
--- src/components/common/BagItem.js
+++ src/components/common/BagItem.js
@@ -6,11 +6,11 @@ import { useEffect } from "react";
 function BagItem({ item }) {
   const { order, setOrder } = useContext(shoppingContext);
   const [values, handleChange] = useForm({ qty: item.qty });
-  
+
   useEffect(() => {
     const itemIndex = order.findIndex(
-        (orderItem) => orderItem.name === item.name
-      );
+      (orderItem) => orderItem.name === item.name
+    );
     const updatedOrder = [...order];
     updatedOrder[itemIndex].qty = Number(values.qty);
     setOrder([...updatedOrder]);
@@ -26,23 +26,6 @@ function BagItem({ item }) {
   return (
     <div className="bag-item-container">
       <div className="bag-item">
-        <div className="pic">
-          <img className="skeleton" src={item.img} alt="bag item" />
-        </div>
-        <div className="text">
-          <h3>{item.name}</h3>
-          <div className="qty">
-            <p>qty:</p>
-            <input
-              type="number"
-              name="qty"
-              value={values.qty}
-              onChange={handleChange}
-            />
-          </div>
-          <p>price: ${item.price}</p>
-          <p>total: ${item.price * item.qty}</p>
-        </div>
         <div className="actions">
           <img
             className="icon"
@@ -50,6 +33,24 @@ function BagItem({ item }) {
             src="../../images/icons/close.png"
             alt="bag"
           />
+        </div>
+        <div className="pic">
+          <img className="skeleton" src={item.img} alt="bag item" />
+        </div>
+        <div className="text">
+          <h3>{item.name}</h3>
+          <p>price: ${item.price}</p>
+        </div>
+        <div className="qty">
+          <input
+            type="number"
+            name="qty"
+            value={values.qty}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="total">
+          <p>${item.price * item.qty}</p>
         </div>
       </div>
     </div>
