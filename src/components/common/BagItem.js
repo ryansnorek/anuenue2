9566887528fe831ca0c655fb285ba6339b674dd3
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-03-21 12:16:08 -0400

    wip

--- src/components/common/BagItem.js
+++ src/components/common/BagItem.js
@@ -2,25 +2,24 @@ function BagItem({ item }) {
   return (
     <div className="bag-item-container">
       <div className="bag-item">
-          <div className="pic">
+        <div className="pic">
           <img className="skeleton" src={item.img} alt="bag item" />
-
-          </div>
+        </div>
         <div className="text">
           <h5>{item.name}</h5>
           <p>qty: {item.qty}</p>
           <p>price: ${item.price}</p>
           <p>total: ${item.price * item.qty}</p>
         </div>
-       <div className="actions">
-        <img
-          className="icon"
-          //   onClick={() => setModalItem("")}
-          src="../../images/icons/close.png"
-          alt="bag"
+        <div className="actions">
+          <img
+            className="icon"
+            //   onClick={() => setModalItem("")}
+            src="../../images/icons/close.png"
+            alt="bag"
           />
           <button>update</button>
-       </div>
+        </div>
       </div>
     </div>
   );
