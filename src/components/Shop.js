351ce652b6e0bf09f2e3bec35528ada356aa82d4
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-03-22 18:49:17 -0400

    shop wip

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -19,7 +19,7 @@ function Shop() {
       <div className="shop">
         {modalItem && <ItemModal item={modalItem} />}
         <section className={`shop-items ${modalItem && "blur"}`}>
-          <h2>Desserts</h2>
+          <h2>D E S S E R T S</h2>
           <div className="item-container">
             <Item item={data.chonky_chip} />
             <Item item={data.based_brownies} />
@@ -27,7 +27,15 @@ function Shop() {
           </div>
         </section>
         <section className={`shop-items ${modalItem && "blur"}`}>
-          <h2>Pepe Choice</h2>
+          <h2>S P E S H A L</h2>
+          <div className="item-container">
+            <Item item={data.power_bitch_balls} />
+            <Item item={data.all_your_breads} />
+            <Item item={data.champs_elycakes} />
+          </div>
+        </section>
+        <section className={`shop-items ${modalItem && "blur"}`}>
+          <h2>M E R C H</h2>
           <div className="item-container">
             <Item item={data.power_bitch_balls} />
             <Item item={data.all_your_breads} />
