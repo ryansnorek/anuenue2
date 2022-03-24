5e66733c6913d988c4681545f7e92b47869168d6
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-03-24 18:07:43 -0400

    server / stripe

--- server/server.js
+++ server/server.js
@@ -1,44 +1,48 @@
 require("dotenv").config();
-
-const storeItems = require("./storeItems");
-
-const cors = require("cors");
 const express = require("express");
 const app = express();
 
-app.use(express.json());
-app.use(express.static("public"));
+const port = process.env.PORT || 8000;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+const storeItems = require("./storeItems");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors({
-  origin: "http://localhost:3001"
+  origin: "http://localhost:3000"
 }))
 
-const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+app.use(express.static(`${__dirname}/public`));
 
-app.post("/create-checkout-session", async (req, res) => {
+app.post("/stripe/charge", cors(), async (req, res) => {
+  const { id } = req.body;
+  // get amount from storeItems
+  const amount = 100;
   try {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      mode: "payment",
-      line_items: req.body.map((item) => {
-        return {
-          price_data: {
-            currency: "usd",
-            product_data: {
-              name: item.name
-            },
-            unit_amount: item.price
-          },
-          quantity: item.qty
-        }
-      }),
-      success_url: `${process.env.SERVER_URL}/checkout`,
-      cancel_url: `${process.env.SERVER_URL}/bag`
-    })
-    res.json({ url: session.url });
+    const payment = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: "USD",
+      description: "Anuenue",
+      payment_method: id,
+      confirm: true,
+    });
+    console.log(payment);
+    res.json({
+      message: "Payment Successful",
+      success: true,
+    });
   } catch (err) {
-    res.status(500).json({ error: err.message })
+    res.json({
+      message: "Payment Failed",
+      success: false,
+    })
   }
 })
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server on ${port}`)
+});
+
 
