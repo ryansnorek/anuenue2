f1cd33cb53ce0762f0c6cb14ff0eecedadea60ee
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-04-01 09:13:00 -0400

    copy to clipboard / css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,21 @@
 function Contact() {
-  const email = "anuenue@pepe.com";
-
+  const copyToClipboard = () => {
+    const copyText = document.getElementById("email");
+    copyText.focus();
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+    alert(`Copied ${copyText.value} to clipboard`);
+  };
   return (
     <section className="contact">
       <div className="info">
-        <p>{email}</p>
+        <input
+          readOnly={true}
+          id="email"
+          value="anuenue@pepe.com"
+          onClick={copyToClipboard}
+        ></input>
       </div>
     </section>
   );
