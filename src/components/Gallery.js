62714863fe4009cd7449aefe80540d5652b3166c
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-04-09 12:42:25 -0400

    edit

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,14 +1,76 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BASE_URL } from "../config";
 
 import { scrollTo } from "../helper";
 
 function Gallery() {
   useEffect(() => scrollTo(0), []);
 
+  const [img, setImg] = useState("");
+  const [items, setItems] = useState([]);
+
+  const [item, setItem] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/store/3`)
+      .then(({ data }) => {
+        setItems(data);
+        const { image } = data;
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const handleSelectFile = (e) => {
+    setImg(e.target.files[0]);
+  };
+  // useEffect(() => {
+  //   const fd = new FormData();
+  //   console.log(img)
+  //   fd.append("image", img)
+  //   axios
+  //     .post(`http://localhost:8000/store/single/3`, fd, {
+  //       onUploadProgress: e => console.log(e.loaded / e.total)
+  //     })
+  //     .then((res) => {
+  //       console.log(res);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, [img])
+
+  // const handleLoad = (e) => {
+  //   setImg(e.target.files[0]);
+  // };
+  // const handleFileUpload = (e) => {
+  //   e.preventDefault();
+
+  //   const updatedItem = {
+  //     ...items[2],
+  //     image: img,
+  //   };
+  //   setItem(updatedItem);
+  // };
   return (
     <div className="gallery">
       <div className="gallery-wrapper">
         <h1>Gallery closed</h1>
+        <input type="file" onChange={handleSelectFile} />
+        <button>upload</button>
+        {/* <form onSubmit={handleFileUpload}>
+          <input
+            type="file"
+            name="pic"
+            accept="image/*"
+            onChange={handleLoad}
+          />
+          <input type="submit" value="Upload"/>
+        </form> */}
       </div>
     </div>
   );
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,4 @@
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { useNavigate } from "react-router";
 
 import { effectsContext } from "../contexts";
@@ -16,6 +16,36 @@ function Landing() {
     setPageTarget(target);
     navigate("/shop");
   };
+
+  // Key 
+  const handleKey = (e) => {
+    if (e.keyCode === 80) {
+      const key = document.getElementById("key");
+      key.classList.remove("hide");
+    }
+  };
+  const handleClickKey = () => {
+    const pass = document.getElementById("pass");
+    pass.classList.remove("hide");
+    pass.focus();
+  };
+  const [pass, setPass] = useState("");
+  const handleChangePass = (e) => {
+    setPass(e.target.value);
+  }
+  const [editMode, setEditMode] = useState(false);
+  const handleClickOk = () => {
+    setEditMode(true);
+  }
+  useEffect(() => {
+    if (pass.length > 3) {
+      const ok = document.getElementById("ok");
+      ok.classList.remove("hide");
+    }
+  }, [pass])
+  document.addEventListener("keydown", handleKey);
+
+  // Key
   return (
     <main className="landing">
       <section className="trending">
@@ -42,9 +72,27 @@ function Landing() {
                 Fancy meeting you here. I'm Erica and this is what I do. I can't
                 wait to show you my delectibles
               </p>
+              <div className="sas">
+                <img
+                  id="key"
+                  className="hide"
+                  src="../images/sas.jpeg"
+                  alt="sas"
+                  onClick={handleClickKey}
+                />
+                <input id="pass" className="hide" name="pass" type="password" value={pass} onChange={handleChangePass}/>
+                <button id="ok" className="hide" onClick={handleClickOk}>ok</button>
+              </div>
             </div>
           </div>
         </div>
+        {editMode && (
+          <>
+          <div className="edit-mode">
+            <h1>EDIT MODE</h1>
+          </div>
+          </>
+        )}
       </section>
       <Footer />
     </main>
