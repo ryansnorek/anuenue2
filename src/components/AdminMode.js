42502c325da62ef42827dd1bfa8aa45e286dfa18
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-04-09 17:02:23 -0400

    wip

--- src/components/AdminMode.js
+++ src/components/AdminMode.js
@@ -10,41 +10,40 @@ function AdminMode({ handleCancelAdmin }) {
     setPic(e.target.files[0]);
   };
   const handleUpload = (id) => {
-      console.log(id)
+    console.log(id);
     setPicID(id);
-  }
+  };
   useEffect(() => {
     axios
       .get(`http://localhost:8000/store`)
       .then((items) => {
         setStoreItems(items.data);
-        console.log(items.data)
+        console.log(items.data);
       })
       .catch((err) => {
         console.log(err);
       });
   }, [pic]);
 
-
   useEffect(() => {
-      console.log(pic, picID, "effect")
+    console.log(pic, picID, "effect");
     if (pic && picID) {
-        const fd = new FormData();
-        fd.append("image", pic)
-        console.log("writing pic........")
-        axios
-          .post(`http://localhost:8000/store/single/${picID}`, fd, {
-            onUploadProgress: e => console.log(e.loaded / e.total)
-          })
-          .then(() => {
-            setPic("");
-            setPicID("");
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+      const fd = new FormData();
+      fd.append("image", pic);
+      console.log("writing pic........");
+      axios
+        .post(`http://localhost:8000/store/single/${picID}`, fd, {
+          onUploadProgress: (e) => console.log(e.loaded / e.total),
+        })
+        .then(() => {
+          setPic("");
+          setPicID("");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
-  }, [pic, picID])
+  }, [pic, picID]);
 
   return (
     <div className="admin-mode">
@@ -60,9 +59,11 @@ function AdminMode({ handleCancelAdmin }) {
             <div className="store-item">
               <h3>{item.name}</h3>
               <div className="uploader">
-              <img src={`http://localhost:8000/${item.pic}`} alt="pic"/>
-                <button onClick={() => handleUpload(item.item_id)}>upload</button>
+                <img src={`http://localhost:8000/${item.pic}`} alt="pic" />
                 <input type="file" onChange={handleSelectFile} />
+                <button onClick={() => handleUpload(item.item_id)}>
+                  upload
+                </button>
               </div>
               <button id="edit">edit</button>
             </div>
