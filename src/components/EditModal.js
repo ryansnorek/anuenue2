6979a4699a1fd90d852605bd135fc01fb960989d
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-04-09 19:16:49 -0400

    edit-modal

--- src/components/EditModal.js
+++ src/components/EditModal.js
@@ -1,15 +1,24 @@
+import useForm from "../hooks/useForm";
+
 function EditModal({ item, setEditItem }) {
+
+  const initialValues = {
+    name: item.name,
+    description: item.description,
+  };
+  const [values, handleChange] = useForm(initialValues);
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
   const handleCloseModal = () => {
     setEditItem(false);
-  }
+  };
   return (
     <section className="edit-modal">
       <form onSubmit={handleSubmit}>
-        <p>Name: {item.name}</p>
-        <p>Description: {item.description}</p>
+        <input value={values.name} onChange={handleChange} />
+        <textarea value={values.description} onChange={handleChange} />
         <button>Update</button>
         <img
           className="icon"
